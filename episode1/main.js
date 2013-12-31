a121cfc78f3fa43a0bbbfaaf7e07815552a195b3
Keith Peters <kp@bit-101.com>
2013-12-31 08:14:40 -0500

    episode 1 sample files

---
+++ episode1/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+		height = canvas.height = window.innerHeight;
+
+	for(var i = 0; i < 100; i += 1) {
+		context.beginPath();
+		context.moveTo(Math.random() * width, Math.random() * height);
+		context.lineTo(Math.random() * width, Math.random() * height);
+		context.stroke();
+	}
+};
